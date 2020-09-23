2b24d9c7b68609f6be02b49388b3c4fca58daf7c
spencermendoza <spencerjmendoza@gmail.com>
2020-09-23 11:50:22 -0400

    mostly finished with the nextSession command, began working on adding the reactions to the message

--- commands/nextSession.js
+++ commands/nextSession.js
@@ -5,12 +5,33 @@ module.exports = {
     name: 'nextsession',
     description: 'this command schedules next session (???)',
     execute(message, args) {
-        var newSession = new Session.Session(args);
+        var newSession = new Session.Session();
+        var newDate = new Date(args);
+        var clientDB = message.client.db;
+
+        let isValidDate = function (date) {
+            return date instanceof Date && !isNaN(date);
+        }
+
+        if (isValidDate(newDate)) {
+            newSession.date = newDate;
+        } else {
+            var today = new Date();
+            today.setHours(11, 00, 00)
+            for (let i = 0; i < 7; i++) {
+                if (today.getDay() === 0) {
+                    break;
+                } else {
+                    today.setDate(today.getDate() + 1);
+                }
+            }
+            newSession.date = today;
+        }
 
         let newEmbed = function (session) {
             let embed = new Discord.MessageEmbed()
                 .setColor(0x1D82B6)
-                .setTitle("Just testing")
+                .setTitle("**NEXT TIME ON DUNGEONS AND DRAGONS**")
                 .addFields(
                     {
                         name: ':calendar_spiral: **Dungeons and Dragons**',
@@ -31,6 +52,7 @@ module.exports = {
                 )
             return embed;
         }
+        clientDB = newSession;
         message.channel.send(newEmbed(newSession))
     }
 }
--- index.js
+++ index.js
@@ -9,11 +9,13 @@ const schedulingTest = '750746205667197048';
 const player = require('./thisPlayer.js');
 client.db = { thisSession: new Date(), nextSessionGood: [], nextSessionNotGood: [], nextSession: {} }
 
+//Just console logs when the bot is online
 client.once('ready', () => {
     var now = new Date()
     console.log('This bot is online at: ', now)
 });
 
+//Creates a collection of commands for this bot to use
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
@@ -21,51 +23,7 @@ for (const file of commandFiles) {
     client.commands.set(command.name, command)
 }
 
-
-//run the date checker
-client.on('ready', () => {
-    setInterval(function () {
-        var caseAnswer = dateThings.dateHandler();
-        console.log('caseanswer: ', caseAnswer)
-        if (caseAnswer === 'case135') {
-            case135();
-        } else if (caseAnswer === 'case6') {
-            case6();
-        } else if (caseAnswer === 'case7') {
-            case7();
-        } else if (caseAnswer === 'case0') {
-            console.log('it is not the right day for me to do something ', caseAnswer)
-        } else {
-            console.log('not working right')
-        }
-    }, 43200000)
-});
-
-let case135 = function () {
-    console.log('its either Monday, Wednesday, or Friday')
-    client.channels.cache.get(schedulingTest).send(`@everyone, Who's good for this Sunday? ${client.db.thisSession.toDateString()}`)
-}
-
-let case6 = function () {
-    console.log('its Saturday');
-    client.channels.cache.get(schedulingTest).send('@everyone, Reminder that we\'re playing tomorrow!')
-
-}
-
-let case7 = function () {
-    console.log('its Sunday!')
-    client.db.thisSession = nextSessionDate();
-    client.channels.cache.get(schedulingTest).send('@everyone, it\'s Sunday! Time to start planning our next session!')
-    client.db.nextSessionGood = [];
-    client.db.nextSessionNotGood = [];
-}
-
-let nextSessionDate = function () {
-    var now = thisSession;
-    now.setDate((now.getDate() + 7));
-    return now;
-}
-
+//Checks if the message starts with '!' and then runs the appropriate command if it exists
 client.on('message', msg => {
     var commandObjs = client.commands;
     if (!msg.content.startsWith(prefix) || msg.author.bot) return;
@@ -88,19 +46,12 @@ client.on('message', msg => {
 });
 
 client.on('message', msg => {
-    if (msg.content.includes('this bot is cool')) {
-        msg.reply("What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little 'clever' comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.")
+    if (msg.author.bot === true && msg.embeds[0].title === '**NEXT TIME ON DUNGEONS AND DRAGONS**') {
+        msg.react(':white_check_mark:')
     }
 })
 
-function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
-};
-
+//When a reaction is added, checks if the message has an embed or not
 client.on('messageReactionAdd', async (reaction, user) => {
     if (reaction.partial) {
         try {
@@ -111,69 +62,131 @@ client.on('messageReactionAdd', async (reaction, user) => {
         }
     }
 
-    var scrim = scrims[reaction.message.id]
-    if (scrim) {
-        scrim.addPlayer(user.id)
-    }
+    console.log(reaction.message.author)
+    console.log(reaction.message.embeds[0])
+
+    // var scrim = scrims[reaction.message.id]
+    // if (scrim) {
+    //     scrim.addPlayer(user.id)
+    // }
 })
 
+//Just makes the bot paste the navy seal copypasta
 client.on('message', msg => {
-    if (msg.content.startsWith('+scrim')) {
-        var playerSlots = 4;
-        var promptText = `Scrim started with ${playerSlots} slots. Add reaction to join.`
-        msg.reply(promptText).then(botMsg => {
-            scrims[botMsg.id] = new Scrim(botMsg, playerSlots)
-        })
+    if (msg.content.includes('this bot is cool')) {
+        msg.reply("What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little 'clever' comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.")
     }
 })
 
-var scrims = {};
-class Scrim {
-    constructor(message, maxPlayers) {
-        this.message = message;
-        this.maxPlayers = maxPlayers;
-        this.players = [];
-    }
+// client.on('message', msg => {
+//     if (msg.content.startsWith('+scrim')) {
+//         var playerSlots = 4;
+//         var promptText = `Scrim started with ${playerSlots} slots. Add reaction to join.`
+//         msg.reply(promptText).then(botMsg => {
+//             scrims[botMsg.id] = new Scrim(botMsg, playerSlots)
+//         })
+//     }
+// })
 
-    addPlayer(id) {
-        this.players.push(id)
-        this.announcePlayerCount();
+// var scrims = {};
+// class Scrim {
+//     constructor(message, maxPlayers) {
+//         this.message = message;
+//         this.maxPlayers = maxPlayers;
+//         this.players = [];
+//     }
 
-        if (this.players.length === this.maxPlayers) {
-            this.handleFullMatch();
-        }
-    }
+//     addPlayer(id) {
+//         this.players.push(id)
+//         this.announcePlayerCount();
 
-    announcePlayerCount() {
-        this.message.channel.send(`There are ${this.players.length} people in the scrim.`)
-    }
+//         if (this.players.length === this.maxPlayers) {
+//             this.handleFullMatch();
+//         }
+//     }
 
-    handleFullMatch() {
-        var teamOne = [];
-        var teamTwo = [];
-        var shuffledPlayers = shuffle([...this.players]);
+//     announcePlayerCount() {
+//         this.message.channel.send(`There are ${this.players.length} people in the scrim.`)
+//     }
 
-        shuffledPlayers.forEach((player, i) => {
-            var tag = '<@' + player + '>';
-            if (i % 2) {
-                teamOne.push(tag)
-            } else {
-                teamTwo.push(tag)
-            }
-        })
+//     handleFullMatch() {
+//         var teamOne = [];
+//         var teamTwo = [];
+//         var shuffledPlayers = shuffle([...this.players]);
 
-        this.message.channel.send([
-            "",
-            `*** SCRIM TEAMS ***`,
-            `Team One: ${teamOne.join(", ")}`,
-            `**VS**`,
-            `Team Two: ${teamTwo.join(", ")}`
-        ]).then(() => {
-            this.message.edit("SCRIM FILLED!")
-            delete scrims[this.message.id];
-        })
-    }
-}
+//         shuffledPlayers.forEach((player, i) => {
+//             var tag = '<@' + player + '>';
+//             if (i % 2) {
+//                 teamOne.push(tag)
+//             } else {
+//                 teamTwo.push(tag)
+//             }
+//         })
+
+//         this.message.channel.send([
+//             "",
+//             `*** SCRIM TEAMS ***`,
+//             `Team One: ${teamOne.join(", ")}`,
+//             `**VS**`,
+//             `Team Two: ${teamTwo.join(", ")}`
+//         ]).then(() => {
+//             this.message.edit("SCRIM FILLED!")
+//             delete scrims[this.message.id];
+//         })
+//     }
+// }
+
+// function shuffle(a) {
+//     for (let i = a.length - 1; i > 0; i--) {
+//         const j = Math.floor(Math.random() * (i + 1));
+//         [a[i], a[j]] = [a[j], a[i]];
+//     }
+//     return a;
+// };
+
+//run the date checker
+// client.on('ready', () => {
+//     setInterval(function () {
+//         var caseAnswer = dateThings.dateHandler();
+//         console.log('caseanswer: ', caseAnswer)
+//         if (caseAnswer === 'case135') {
+//             case135();
+//         } else if (caseAnswer === 'case6') {
+//             case6();
+//         } else if (caseAnswer === 'case7') {
+//             case7();
+//         } else if (caseAnswer === 'case0') {
+//             console.log('it is not the right day for me to do something ', caseAnswer)
+//         } else {
+//             console.log('not working right')
+//         }
+//     }, 43200000)
+// });
+
+// let case135 = function () {
+//     console.log('its either Monday, Wednesday, or Friday')
+//     client.channels.cache.get(schedulingTest).send(`@everyone, Who's good for this Sunday? ${client.db.thisSession.toDateString()}`)
+// }
+
+// let case6 = function () {
+//     console.log('its Saturday');
+//     client.channels.cache.get(schedulingTest).send('@everyone, Reminder that we\'re playing tomorrow!')
+
+// }
+
+// let case7 = function () {
+//     console.log('its Sunday!')
+//     client.db.thisSession = nextSessionDate();
+//     client.channels.cache.get(schedulingTest).send('@everyone, it\'s Sunday! Time to start planning our next session!')
+//     client.db.nextSessionGood = [];
+//     client.db.nextSessionNotGood = [];
+// }
+
+// let nextSessionDate = function () {
+//     var now = thisSession;
+//     now.setDate((now.getDate() + 7));
+//     return now;
+// }
 
 // var newSession = {};
 // let createNextSession = function (date) {
